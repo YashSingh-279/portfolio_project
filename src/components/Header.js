e715fe66860060bce7a4c7219868d586ef73e2da
Yash <1928279@kiit.ac.in>
2021-06-20 23:08:41 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div className= "Header-wraper">
+            <div className="main-info">
+                <hi>NEWBIE TO FRONT END DEV AND UI DESIGNER</hi>
+
+         <a href="#" className="btn-main-offer">contact me</a>
+                
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
